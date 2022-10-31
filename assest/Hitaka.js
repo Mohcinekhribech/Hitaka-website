1c497c51cd506336f7925c54e47ff518765d1e09
Mohcinekhribech <mohcinekhribche123@gmail.com>
2022-10-31 17:42:59 +0100

    Livrable 3

--- assest/Hitaka.js
+++ assest/Hitaka.js
@@ -7,7 +7,8 @@ var i=0;
 var sum=0;
 var text=[];
 var txt=[];
-
+var idindex = Array();
+for(let z=0;z<16;z++) idindex[z]=0;
 
 console.log(xx);
 bars.addEventListener("click",e => {
@@ -47,14 +48,27 @@ function a() {
         document.getElementById('obento').style.display="block";
     }
 }
-function clickOne(price, name){
+function clickOne(price, name,id){
     sum+=price;
     if(document.getElementById("not")!=undefined){
-        document.getElementById("not").style.display="none"
-    }
+            document.getElementById("not").style.display="none"
+           }
     const cont = document.getElementById("cont")
-    txt[i]=name;
-    cont.innerHTML+=`<p class="Sprdt">${name}</p>`
+          txt[i]=name;
+    for(let y=0;y<16;y++){
+        if(id==y+1&&idindex[y]==0){
+          cont.innerHTML+=`<p class="Sprdt">(<span id="${id}">1</span>)${name}</p>`
+          document.getElementById(id).innerHTML=idindex[y]+1
+            idindex[y]++
+          
+          console.log(idindex[y])
+          
+        }else if(id==y+1&&idindex[y]>=0){
+            document.getElementById(id).innerHTML=idindex[y]+1
+            idindex[y]++;
+            console.log(idindex[y])
+        }
+    }
     i++;
     document.querySelector("#pcont").innerText=i;
 }
@@ -70,11 +84,18 @@ document.getElementById("fa-recycle").addEventListener("click",e =>{
     var sup = document.getElementsByClassName("Sprdt")
     sum =0;
     i=0;
+    for(let z=0;z<16;z++) idindex[z]=0;
     document.getElementById("Tprice").innerText = sum;
     document.getElementById("mvalId").innerText = sum;
     document.querySelector("#pcont").innerText=i;
     document.getElementById("not").style.display="block"
-    for(let j=sup.length-1;j>=0;j++){
-      sup[sup.length-1].remove()
+    for(let j=sup.length-1;j>=0;j--){
+      sup[j].remove()
     }
 })
+function msg(){
+    document.getElementById("mcn").style.display="block"
+}
+function f(){
+    document.getElementById("mcn").style.display="none"
+}
