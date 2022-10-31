d0cc0975dffe580693941ad0aef798b17df6e447
Mohcinekhribech <mohcinekhribche123@gmail.com>
2022-10-31 11:23:34 +0100

    Livrable 3

--- assest/Hitaka.js
+++ assest/Hitaka.js
@@ -5,7 +5,8 @@ const shop = document.getElementById("fa-bag-shopping")
 const sel = document.getElementsByTagName("select")
 var i=0;
 var sum=0;
-var text=[]
+var text=[];
+var txt=[];
 
 
 console.log(xx);
@@ -48,19 +49,32 @@ function a() {
 }
 function clickOne(price, name){
     sum+=price;
-    console.log(sum);
     if(document.getElementById("not")!=undefined){
-        document.getElementById("not").remove()
+        document.getElementById("not").style.display="none"
     }
-    
     const cont = document.getElementById("cont")
-    var p = document.createElement("p")
-    text[i]=document.createTextNode(name)
-        p.appendChild(text[i])
-    cont.appendChild(p)
+    txt[i]=name;
+    cont.innerHTML+=`<p class="Sprdt">${name}</p>`
     i++;
+    document.querySelector("#pcont").innerText=i;
 }
+document.getElementById("xx-x").addEventListener("click",e => {
+    document.getElementById("valider").style.display="none"
+})
 function Totale (){
+    document.getElementById("valider").style.display="block"
     document.getElementById("Tprice").innerText = sum;
+    document.getElementById("mvalId").innerText = sum;
 }
-
+document.getElementById("fa-recycle").addEventListener("click",e =>{
+    var sup = document.getElementsByClassName("Sprdt")
+    sum =0;
+    i=0;
+    document.getElementById("Tprice").innerText = sum;
+    document.getElementById("mvalId").innerText = sum;
+    document.querySelector("#pcont").innerText=i;
+    document.getElementById("not").style.display="block"
+    for(let j=sup.length-1;j>=0;j++){
+      sup[sup.length-1].remove()
+    }
+})
